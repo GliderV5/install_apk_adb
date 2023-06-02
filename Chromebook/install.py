1a532b5442d4ae5fcd04ae684f29ad67a8bd9bf8
Aurélien Dubois <gliderperso@gmail.com>
2023-06-02 13:49:28 +0200

    amélioration

--- Chromebook/install.py
+++ Chromebook/install.py
@@ -6,10 +6,20 @@ import subprocess
 try:
     output = subprocess.check_output(["adb", "version"])
 except subprocess.CalledProcessError as e:
-    print("adb is not installed or there is a problem. Try the command <sudo apt-get install android-sdk-platform-tools>")
-    print("press enter to exit ...")
-    input()
-    exit()
+    answer = input("adb is not installed on your computer. Do you want to install it now ? (y,n) >>> ")
+    if answer == "yes":
+        try:
+            os.system("sudo apt-get install android-sdk-platform-tools -y")
+            print()
+            print("adb is now installed on your computer your app will be installed soon, please wait ...")
+            time.sleep(5)
+            os.system("clear")
+        except:
+            print()
+            print("there was an error during installation")
+            exit()
+    else:
+        exit()
 
 try:
     os.system("clear")
